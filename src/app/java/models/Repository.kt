e924a30d47a94639195e83bd27a33706ed496c25
MAINTIFE <oluwaniyiboluwatife535@gmail.com>
2024-07-04 09:30:09 +0100

    updates made

--- src/app/java/models/Repository.kt
+++ src/app/java/models/Repository.kt
@@ -3,50 +3,44 @@ package app.java.models
 import java.util.*
 
 object Repository {
-    private var users: MutableList<User>? = null
+    private val _users = mutableListOf<User>()
+    val users: List<User>
+        get() = _users
+
+
+    val formattedUserNames: List<String>
+        get() {
+            return _users.map { user -> user.userFormattedName}
+        }
+
 
     // keeping the constructor private to enforce the usage of getInstance
     init {
         val user1 = User("Jane", "")
         val user2 = User("John", null)
         val user3 = User("Anne", "Doe")
-        users.add(user1)
-        users.add(user2)
-        users.add(user3)
-    }
 
-    fun getUsers(): List<User>? {
-        return users
-    }
-
-    val formattedUserNames: List<String>
-        get() {
-            return users.map { user ->
-                if (user.lastName != null) {
-                    if (user.firstName != null) {
-//                        firstName + " " + lastName (instead of this you can use the string template below)
-                        "${user.firstName} ${user.lastName}"
-                    } else {
-                        user.lastName ?: "Unknown"
-                    }
-                } else {
-                    user.firstName ?: "Unknown"
-                }
-            }
+        _users.apply {
+            // this == _users
+            add(user1)
+            add(user2)
+            add(user3)
         }
-
-    companion object {
-        private var INSTANCE: Repository? = null
-        val instance: Repository?
-            get() {
-                if (INSTANCE == null) {
-                    synchronized(Repository::class.java) {
-                        if (INSTANCE == null) {
-                            INSTANCE = Repository()
-                        }
-                    }
-                }
-                return INSTANCE
-            }
     }
+
+//    companion object {
+//        private var INSTANCE: Repository? = null
+//        val instance: Repository?
+//            get() {
+//                if (INSTANCE == null) {
+//                    synchronized(Repository::class.java) {
+//                        if (INSTANCE == null) {
+//                            INSTANCE = Repository()
+//                        }
+//                    }
+//                }
+//                return INSTANCE
+//            }
+//    }
 }
+
--- src/app/java/models/User.kt
+++ src/app/java/models/User.kt
@@ -7,3 +7,37 @@ data class User(var firstName: String?, var lastName: String? = null)
 // usage
 val jane = User("Jane") // same as User("Jane", null)
 val joe = User(firstName =  "Joe", lastName =  "Doe")
+
+
+
+// extension function
+fun User.getFormattedName(): String {
+    return if (lastName != null) {
+        if (firstName != null) {
+            "$firstName $lastName"
+        } else {
+            lastName ?: "Unknown"
+        }
+    } else {
+        firstName ?: "Unknown"
+    }
+}
+
+// extension property
+val User.userFormattedName: String
+    get() {
+        return if (lastName != null) {
+            if (firstName != null) {
+                "$firstName $lastName"
+            } else {
+                lastName ?: "Unknown"
+            }
+        } else {
+            firstName ?: "Unknown"
+        }
+    }
+
+// usage:
+val user = User("Alice", "Smith")
+val name = user.getFormattedName()
+val formattedName = user.userFormattedName
